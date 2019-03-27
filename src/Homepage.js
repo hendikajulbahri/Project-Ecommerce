06a28cbda8ccf6318f168565f1d81b17da14a608
hendikajulbahri <julbahrih@gmail.com>
2019-03-27 22:07:32 +0700

    test merge

--- src/Homepage.js
+++ src/Homepage.js
@@ -23,7 +23,7 @@ class Homepage extends Component {
                     <a href="https://www.wellborncompany.com/" class="shopping-guide">Jualan</a>
                   </li>
                   <li class="user-menu">
-                    <a href="https://www.wellborncompany.com/">Login / Sign Up</a>
+                    <a href="https://www.wellborncompany.com/">Login Hendika</a>
                   </li>
 
                 </ul>
